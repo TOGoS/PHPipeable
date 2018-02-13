3c72abf9c55244ba4d621fe6b0a3b642e9e694d1
TOGoS <togos00@gmail.com>
2018-02-13 15:44:27 -0600

    Simplify implementation of SinkGears#__invoke

--- src/main/php/TOGoS/PHPipeable/SinkGears.php
+++ src/main/php/TOGoS/PHPipeable/SinkGears.php
@@ -2,8 +2,7 @@
 
 trait TOGoS_PHPipeable_SinkGears
 {
-	public function __invoke() {
-		$args = func_get_args();
-		call_user_func_array(array($this,$this->item), $args);
+	public function __invoke($item, array $metadata=array()) {
+		$this->item($item, $metadata);
 	}
 }
