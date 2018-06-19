1f3870d0b4fad3033c6331fd8f584e15a6000c63
TOGoS <togos00@gmail.com>
2018-06-19 18:44:18 -0500

    Add 'onError' option to Filter (untested)

--- src/main/php/TOGoS/PHPipeable/Filter.php
+++ src/main/php/TOGoS/PHPipeable/Filter.php
@@ -10,12 +10,32 @@ extends TOGoS_PHPipeable_AbstractFilter
 	 * then nulls returned by $filterCallback
 	 * will be 'dropped on the floor' rather than emitted.
 	 */
-	public function __construct($filterCallback, $dropNulls=false) {
+	public function __construct($filterCallback, $options=false) {
 		$this->filterCallback = $filterCallback;
-		$this->dropNulls = $dropNulls;
+		if( is_bool($options) ) $options = array(
+			'dropNulls' => $options
+		);
+		$this->dropNulls = isset($options['dropNulls']) ? $options['dropNulls'] : false;
+		$this->onError = isset($options['onError']) ? $options['onError'] : 'throw';
 	}
 	public function item($item, array $metadata=array()) {
-		$value = call_user_func($this->filterCallback,$item,$metadata);
+		try {
+			$value = call_user_func($this->filterCallback,$item,$metadata);
+		} catch( Exception $e ) {
+			switch( $this->onError ) {
+			case 'skip':
+				return;
+			case 'pass':
+				$this->emitItem(null, array(
+					'errors' => array(
+						array('message'=>"Exception while transforming", 'exception' => $e)
+					),
+				) + $metadata);
+				return;
+			default: // 'throw'
+				throw $e;
+			}
+		}
 		if( $value === null && $this->dropNulls ) return;
 		$this->emitItem($value,$metadata);
 	}
